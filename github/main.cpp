2a20c64b17801040cd84697eeec9182ad3e6b5c3
Snowhan1021 <smicjoonhohan@hotmail.com>
2020-04-03 15:50:19 +0900

    Initial

---
+++ github/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  github
+//
+//  Created by Junho on 2020/04/03.
+//  Copyright © 2020 Junho. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    // insert code here...
+    cout << "Hello, World!\n";
+    cout << "Hello Git";
+    
+}
